d6b421383f0dce0a7688a1815b4b06abbabebded
Micro Fortwonatus <microfortwonatus6@gmail.com>
2024-11-30 16:14:10 +0700

    hero responsive

--- src/components/portofolio/productData.jsx
+++ src/components/portofolio/productData.jsx
@@ -3,6 +3,9 @@ const productData = [
     id: 1,
     Title: "Peremajaan Identitas Visual Tunggal Computer Magelang",
     img: "../assets/portofolio/top/porto-dds_g.jpg",
+    tujuan: "Merevitalisasi merek: Menciptakan identitas visual yang segar, modern, dan lebih mencerminkan nilai-nilai inti Tunggal Computer. Meningkatkan relevansi: Memastikan identitas baru dapat terhubung dengan target audiens yang lebih luas, terutama generasi muda. Memperkuat positioning: Menegaskan posisi Tunggal Computer sebagai pemimpin pasar di bidang komputer dan aksesoris di wilayah Jawa Tengah. Meningkatkan daya tarik: Membuat merek Tunggal Computer lebih menarik dan mudah diingat.",
+    tantangan: "",
+    implementasi: "",
     des: "Transformasi Tunggal Computer: Meremajakan Identitas untuk Dominasi Pasar Dalam lanskap bisnis teknologi yang terus berkembang, membangun identitas merek yang kuat dan relevan menjadi kunci kesuksesan. Tunggal Computer, toko komputer dan aksesoris terkemuka di Jawa Tengah, menyadari pentingnya hal ini dan menginisiasi proyek peremajaan identitas visual yang komprehensif Tantangan dan Tujuan Seiring berjalannya waktu, identitas visual Tunggal Computer dianggap kurang relevan dengan tren desain masa kini. Hal ini berpotensi menghambat daya tarik merek di kalangan konsumen, terutama generasi muda yang lebih melek teknologi. Oleh karena itu, proyek ini bertujuan untuk: Merevitalisasi merek: Menciptakan identitas visual yang segar, modern, dan lebih mencerminkan nilai-nilai inti Tunggal Computer. Meningkatkan relevansi: Memastikan identitas baru dapat terhubung dengan target audiens yang lebih luas, terutama generasi muda. Memperkuat positioning: Menegaskan posisi Tunggal Computer sebagai pemimpin pasar di bidang komputer dan aksesoris di wilayah Jawa Tengah. Meningkatkan daya tarik: Membuat merek Tunggal Computer lebih menarik dan mudah diingat."
   },
 
